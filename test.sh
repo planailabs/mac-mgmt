22c4d0586b2805e37ff12ddfe3fb12a542aa2101
Maciej Krüger <mkg20001@gmail.com>
2026-03-23 14:01:50 +0100

    fix: test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,6 @@ set -euxo pipefail
 
 IP="89.167.85.155"
 cargo build --release --target x86_64-unknown-linux-musl
-scp target/x86_64-unknown-linux-musl/release/mac-mgmt root@$IP:
+scp target/x86_64-unknown-linux-musl/release/mac-mgmt root@$IP:mac-mgmt-new
+ssh root@$IP mv mac-mgmt-new mac-mgmt
 ssh root@$IP ./mac-mgmt "$@"
