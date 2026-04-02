10766043bc9982a0d369fa2d4670cf825f18a3fc
Maciej Krüger <mkg20001@gmail.com>
2026-04-02 11:17:29 +0200

    feat: new test server

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-IP="89.167.85.155"
+IP="2a01:4f9:1a:90eb::cd"
 cargo build --release --target x86_64-unknown-linux-musl -p mac-mgmt
 ssh root@$IP rm -f mac-mgmt-new
 scp target/x86_64-unknown-linux-musl/release/mac-mgmt root@$IP:mac-mgmt-new
