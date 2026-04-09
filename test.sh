ae50aab54544d63d414f578964a27f8087d5900f
Maciej Krüger <mkg20001@gmail.com>
2026-04-09 20:27:47 +0200

    fix: test.sh use zigbuild on mac

--- test.sh
+++ test.sh
@@ -3,7 +3,11 @@
 set -euxo pipefail
 
 IP="2a01:4f9:1a:90eb::cd"
-cargo build --release --target x86_64-unknown-linux-musl -p mac-mgmt
+if [ "$(uname)" != "Darwin" ]; then
+  cargo build --release --target x86_64-unknown-linux-musl -p mac-mgmt
+else
+  cargo zigbuild --release --target x86_64-unknown-linux-musl -p mac-mgmt
+fi
 ssh root@$IP rm -f mac-mgmt-new
 scp target/x86_64-unknown-linux-musl/release/mac-mgmt root@[$IP]:mac-mgmt-new
 ssh root@$IP mv mac-mgmt-new mac-mgmt
