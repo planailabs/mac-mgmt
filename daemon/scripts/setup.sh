8b9b7867b8b252f8620d782264c5fcb15d7d6f5b
Maciej Krüger <mkg20001@gmail.com>
2026-04-11 23:04:15 +0200

    fix: use sudo for setup.sh

--- daemon/scripts/setup.sh
+++ daemon/scripts/setup.sh
@@ -4,10 +4,10 @@
 
 yes | sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
 
-echo "extra-experimental-features = nix-command flakes" | tee -a /etc/nix/nix.conf
+echo "extra-experimental-features = nix-command flakes" | sudo tee -a /etc/nix/nix.conf
 
-echo "substituters = https://cache.nixos.org/ https://xzar.plan.ai" | tee -a /etc/nix/nix.conf
-echo "trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= xzar.plan.ai:KUE66pjr6UX5HHCn9kedN1DJ2J5nSlBrKmE7tUjXewE=" | tee -a /etc/nix/nix.conf
+echo "substituters = https://cache.nixos.org/ https://xzar.plan.ai" | sudo tee -a /etc/nix/nix.conf
+echo "trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= xzar.plan.ai:KUE66pjr6UX5HHCn9kedN1DJ2J5nSlBrKmE7tUjXewE=" | sudo tee -a /etc/nix/nix.conf
 
 if [ "$(uname)" != "Darwin" ]; then
   sudo systemctl restart nix-daemon
