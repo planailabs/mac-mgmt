fdb5f5c8d7fd6fbb86b91212b7e639e01c7109bd
Maciej Krüger <mkg20001@gmail.com>
2026-04-17 11:10:49 +0200

    feat: enable relay by default in prod

--- xzar.sh
+++ xzar.sh
@@ -13,7 +13,7 @@ export ENVIRONMENT=production
 # index it and the daemon can `nix-store --realise` it on update.
 
 DAEMON_VERSION="$(grep '^version' "$SCRIPT_DIR/daemon/Cargo.toml" | head -1 | cut -d'"' -f2)"
-FEATURES="self-update,services"
+FEATURES="self-update,services,relay"
 
 upload() {
   ~/.cargo/bin/xzar --server planai upload --pin "$1" --desc $(readlink -f "$2") --leave-after-abandon 1m "$2"
